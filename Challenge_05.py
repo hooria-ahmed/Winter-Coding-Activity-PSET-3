0d039dede7d5a6de10b57fff5ce39c28ca621e99
Hooria Ahmed <hooria.2006.haq@gmail.com>
2026-01-18 05:45:30 +0000

    approcah 2

--- Challenge_05.py
+++ Challenge_05.py
@@ -1,24 +1,35 @@
-def longest_palindrome_basic(s):
+def longest_palindrome_optimized(s):
+    if not s:
+        return ""
+
+    start = 0
+    max_len = 1
     n = len(s)
-    best = ""
+
+    def expand(l, r):
+        nonlocal start, max_len
+        while l >= 0 and r < n and s[l] == s[r]:
+            if r - l + 1 > max_len:
+                start = l
+                max_len = r - l + 1
+            l -= 1
+            r += 1
 
     for i in range(n):
-        for j in range(i, n):
-            sub = s[i:j+1]
-            if sub == sub[::-1] and len(sub) > len(best):
-                best = sub
+        expand(i, i)       # odd length
+        expand(i, i + 1)   # even length
 
-    return best
+    return s[start:start + max_len]
 
 
 s = "babad"
-print(longest_palindrome_basic(s))
+print(longest_palindrome_optimized(s))
 # Expected: "bab" or "aba"
 
 s = "cbbd"
-print(longest_palindrome_basic(s))
+print(longest_palindrome_optimized(s))
 # Expected: "bb"
 
 s = "a"
-print(longest_palindrome_basic(s))
+print(longest_palindrome_optimized(s))
 # Expected: "a"
