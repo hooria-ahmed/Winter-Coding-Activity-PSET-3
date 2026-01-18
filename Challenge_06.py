414e87c085cff8589b0b9a8cde65e1136df65d96
Hooria Ahmed <hooria.2006.haq@gmail.com>
2026-01-18 05:51:10 +0000

    appraoch 2

--- Challenge_06.py
+++ Challenge_06.py
@@ -1,21 +1,23 @@
-def repeated_n_times_basic(nums):
-    freq = {}
-    n = len(nums) // 2
+def repeated_n_times_optimized(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] == nums[i + 1]:
+            return nums[i]
 
-    for x in nums:
-        freq[x] = freq.get(x, 0) + 1
-        if freq[x] == n:
-            return x
+    for i in range(len(nums) - 2):
+        if nums[i] == nums[i + 2]:
+            return nums[i]
+
+    return nums[0]
 
 
 nums = [2, 1, 2, 5, 3, 2]
-print(repeated_n_times_basic(nums))
+print(repeated_n_times_optimized(nums))
 # Expected: 2
 
 nums = [5, 1, 5, 2, 5, 3]
-print(repeated_n_times_basic(nums))
+print(repeated_n_times_optimized(nums))
 # Expected: 5
 
 nums = [7, 7, 7, 4, 1, 2]
-print(repeated_n_times_basic(nums))
+print(repeated_n_times_optimized(nums))
 # Expected: 7
