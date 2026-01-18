33404752f04b50b71c59acced1f268363b685f77
Hooria Ahmed <hooria.2006.haq@gmail.com>
2026-01-18 05:37:17 +0000

    approach 2

--- Challenge_04.py
+++ Challenge_04.py
@@ -1,37 +1,40 @@
-def find_anagrams_basic(s, p):
+def find_anagrams_optimized(s, p):
     res = []
-    p_count = {}
+    if len(p) > len(s):
+        return res
+
+    p_count = [0] * 26
+    win_count = [0] * 26
 
     for ch in p:
-        p_count[ch] = p_count.get(ch, 0) + 1
+        p_count[ord(ch) - ord('a')] += 1
 
     m = len(p)
 
-    for i in range(len(s) - m + 1):
-        window = s[i:i+m]
-        w_count = {}
+    for i in range(len(s)):
+        win_count[ord(s[i]) - ord('a')] += 1
 
-        for ch in window:
-            w_count[ch] = w_count.get(ch, 0) + 1
+        if i >= m:
+            win_count[ord(s[i - m]) - ord('a')] -= 1
 
-        if w_count == p_count:
-            res.append(i)
+        if win_count == p_count:
+            res.append(i - m + 1)
 
     return res
 
 
 s = "cbaebabacd"
 p = "abc"
-find_anagrams_basic(s, p)
+print(find_anagrams_optimized(s, p))
 # Expected: [0, 6]
 
 s = "abab"
 p = "ab"
-find_anagrams_basic(s, p)
+print(find_anagrams_optimized(s, p))
 # Expected: [0, 1, 2]
 
 s = "aaaaa"
 p = "aa"
-find_anagrams_basic(s, p)
+print(find_anagrams_optimized(s, p))
 # Expected
 # : [0, 1, 2, 3]
